1741da5a13ddf7494ec7f2558504c4c036013e97
avtss <avetiszadikyann@mail.ru>
2024-09-13 18:13:12 +0300

    Sum of non-prime dividers function

--- mathmethods.rb
+++ mathmethods.rb
@@ -1,5 +1,5 @@
 def is_prime(num)
-  return false if num < 2  # Простые числа начинаются с 2
+  return false if num < 2
 
   c = 0
   (1..num).each do |i|
@@ -9,4 +9,14 @@ def is_prime(num)
   end
 
   c == 2
+end
+
+def sum_of_nonprime_dividers(num)
+  sum = 0
+  (1..num).each do |i|
+    if num % i == 0 && !is_prime(i)
+      sum += i
+    end
+  end
+  sum
 end
