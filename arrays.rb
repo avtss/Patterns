348896d2ba08587e979837f23b8a928943527557
avtss <avetiszadikyann@mail.ru>
2024-09-18 16:04:50 +0300

    Read from file function

--- arrays.rb
+++ arrays.rb
@@ -1,25 +1,37 @@
-def find_min(arr)
-  min_value = arr[0]
-  for i in 1...arr.length
-    if arr[i] < min_value
-      min_value = arr[i]
+def find_min(ar)
+  min_value = ar[0]
+  for i in 1...ar.length
+    if ar[i] < min_value
+      min_value = ar[i]
     end
   end
   min_value
 end
-def find_max
-  max_value = arr[0]
-  for i in 1...arr.length
-    if arr[i] > max_value
-      max_value = arr[i]
+def find_max(ar)
+  max_value = ar[0]
+  for i in 1...ar.length
+    if ar[i] > max_value
+      max_value = ar[i]
     end
   end
   max_value
 end
-def first_found(arr)
-  for i in 0...arr.length
-    return i if arr[i] > 0
+def first_positive_found(ar)
+  for i in 0...ar.length
+    return i if ar[i] > 0
   end
   -1 
 end
+def read(path)
+  begin
+    file = File.open(path, "r")
+    ar = file.read.split.map(&:to_i)
+    file.close
+    ar
+  rescue => e
+    puts "Ошибка при чтении файла: #{e.message}"
+    []
+  end
+end
+
 
