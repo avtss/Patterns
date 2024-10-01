5117aeca0d4bc9934db1791a623b3d4e90407060
avtss <avetiszadikyann@mail.ru>
2024-10-01 12:44:40 +0300

    3. Constructor can be used with any combination of unnecesarry fields

--- Students/student.rb
+++ Students/student.rb
@@ -12,16 +12,14 @@ class Student
 		@email=email
 		@github=github
 	end
-	def to_s
-		"ID: #{@id}, ФИО: #{@lastname} #{@firstname} #{surname}, номер телефона: #{@phone}, tg: #{@telegram}, email: #{@email}, github: #{@github}"
-	end
+	
 	def display_info
     	puts "Информация о студенте:"
     	puts "ID: #{@id}"
     	puts "ФИО: #{@lastname} #{@firstname} #{@surname}"
-    	puts "Телефон: #{@phone}"
-    	puts "Telegram: #{@telegram}"
-    	puts "Email: #{@email}"
-    	puts "GitHub: #{@github}"
-  	end
+    	puts "Телефон: #{@phone}" if @phone
+    	puts "Telegram: #{@telegram}" if @telegram
+    	puts "Email: #{@email}" if @email
+    	puts "GitHub: #{@github}" if @github
+  end
 end
