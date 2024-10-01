744347a2d240058d26629844a31b55f6c0e16180
avtss <avetiszadikyann@mail.ru>
2024-10-01 12:55:41 +0300

    4. Added method to check if a string is a phone number, modified constructor

--- Students/student.rb
+++ Students/student.rb
@@ -2,17 +2,30 @@ class Student
 	attr_reader :id, :lastname, :firstname, :surname
 	attr_accessor :phone, :telegram, :email, :github
 
+	PHONE_REGEX = /^\+7\d{10}$/
+
+	def self.valid_phone?(phone)
+    	phone =~ PHONE_REGEX
+ 	end
+
+
+ 	def phone=(number)
+    	raise ArgumentError, "Некорректный номер телефона" unless self.class.valid_phone?(number)
+    	@phone = number
+  	end
+  	
 	def initialize(id:, lastname:, firstname:, surname:, phone:nil, telegram:nil, email:nil, github:nil)
 		@id=id
 		@lastname=lastname
 		@firstname=firstname
 		@surname=surname
-		@phone=phone
+		self.phone = phone
 		@telegram=telegram
 		@email=email
 		@github=github
 	end
 	
+
 	def display_info
     	puts "Информация о студенте:"
     	puts "ID: #{@id}"
