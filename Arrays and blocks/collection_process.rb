86f2c3d6d7591ee995f5bd600bd9c523e3d53479
avtss <avetiszadikyann@mail.ru>
2024-11-01 17:07:14 +0300

    Lab 3.1 Check if element is local maximum

--- Arrays and blocks/collection_process.rb
+++ Arrays and blocks/collection_process.rb
@@ -3,5 +3,17 @@ def before_first_min
 	puts "Введите элементы массива через пробел"
 	arr = gets.chomp.split.map(&:to_i)
 	result = arr.take_while { |x| x!= arr.min }
-	puts "Элементы, находящиеся перед первым минимальным: #{result}"
 end
+
+# Проверка, является ли элемент локальным максимумом
+def local_max?
+	puts "Введите элементы массива через пробел"
+	arr = gets.chomp.split.map(&:to_i)
+	puts "Введите индекс, по которому нужно проверить элемент на локальный максимум"
+	ind = gets.chomp.to_i
+	if ind <= 0 || ind >= arr.size - 1
+		puts "Введен неверный индекс"
+		return
+	end
+	result = arr[ind] > arr[ind-1] && arr[ind] > arr[ind+1]
+
