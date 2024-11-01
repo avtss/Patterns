83fc630c1887116a4200fb3ef48025dae380afca
avtss <avetiszadikyann@mail.ru>
2024-11-01 16:39:06 +0300

    Lab 3. 1.1 Find elements before the first minimum

---
+++ Arrays and blocks/collection_process.rb
@@ -0,0 +1,8 @@
+# Поиск элементов, расположенных перед первым минимальным
+def before_first_min
+	puts "Введите элементы массива через пробел"
+	arr = gets.chomp.split.map(&:to_i)
+	result = arr.take_while { |x| x!= arr.min }
+	puts "Элементы, находящиеся перед первым минимальным: #{result}"
+end
+before_first_min()
