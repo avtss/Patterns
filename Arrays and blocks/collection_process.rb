7a3df3e3abf34467d2f98632f14c52b8bc9ae36c
avtss <avetiszadikyann@mail.ru>
2024-11-11 13:09:28 +0300

    Lab3. Moved puts to main function

--- Arrays and blocks/collection_process.rb
+++ Arrays and blocks/collection_process.rb
@@ -1,35 +1,21 @@
 # Поиск элементов, расположенных перед первым минимальным
-def before_first_min
-	puts "Введите элементы массива через пробел"
-	arr = gets.chomp.split.map(&:to_i)
+def before_first_min(arr)
 	result = arr.take_while { |x| x!= arr.min }
 end
 
 # Проверка, является ли элемент локальным максимумом
-def local_max?
-	puts "Введите элементы массива через пробел"
-	arr = gets.chomp.split.map(&:to_i)
-	puts "Введите индекс, по которому нужно проверить элемент на локальный максимум"
-	ind = gets.chomp.to_i
-	if ind <= 0 || ind >= arr.size - 1
-		puts "Введен неверный индекс"
-		return
-	end
+def local_max?(arr, ind)
 	result = arr[ind] > arr[ind-1] && arr[ind] > arr[ind+1]
 end
 
 # Поиск элементов меньше среднего арифметического
-def less_than_average
-	puts "Введите элементы массива через пробел"
-	arr = gets.chomp.split.map(&:to_i)
+def less_than_average(arr)
 	average = arr.sum.to_f / arr.size
 	result = arr.select { |x| x<average}
 end
 
 # Поиск элементов, встречающихся более 3 раз
-def more_than_three_times
-	puts "Введите элементы массива через пробел"
-	arr = gets.chomp.split.map(&:to_i)
+def more_than_three_times(arr)
 	result = arr.select {|x| arr.count(x) > 3}.uniq
 end
 
@@ -44,18 +30,30 @@ loop do
 
 	case choice
 	when 1
-		puts before_first_min
-
+		puts "Введите элементы массива через пробел"
+		arr = gets.chomp.split.map(&:to_i)
+		puts before_first_min(arr)
 	when 2
-		if local_max?
+		puts "Введите элементы массива через пробел"
+		arr = gets.chomp.split.map(&:to_i)
+		puts "Введите индекс, по которому нужно проверить элемент на локальный максимум"
+		ind = gets.chomp.to_i
+		if ind <= 0 || ind >= arr.size - 1
+			raise ArgumentError, "Неправильный индекс"
+		end
+		if local_max?(arr, ind)
 			puts "Элемент под этим индексом является локальным максимумом"
 		else
-			puts "Элемент под этим индексом является локальным максимумом"
+			puts "Элемент под этим индексом не является локальным максимумом"
 		end
 	when 3
-		puts less_than_average
+		puts "Введите элементы массива через пробел"
+		arr = gets.chomp.split.map(&:to_i)
+		puts "Элементы массива меньше среднего: #{less_than_average(arr)}" 
 	when 4
-		puts more_than_three_times
+		puts "Введите элементы массива через пробел"
+		arr = gets.chomp.split.map(&:to_i)
+		puts "Элементы, встречающиеся больше 3 раз: #{more_than_three_times(arr)}"
 	when 0
 		puts "Завершение программы"
 		break
