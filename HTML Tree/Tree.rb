7cffed34e70684dc4e276ac61aecd86e6640bfb5
avtss <avetiszadikyann@mail.ru>
2024-11-13 19:05:22 +0300

    Lab3.3. Added dfs and bfs to tree class

--- HTML Tree/Tree.rb
+++ HTML Tree/Tree.rb
@@ -7,15 +7,33 @@ class Tree
 		@root = root
 	end
 
-	def each(&block)
-	    dfs(&block)
-	end
-
 	def add_child(node, parent = nil)
 	    if parent.nil?
 	    	@root = node
 	    else
 	      	parent.add_child(node)
+	    end
+	end
+
+	def bfs
+		return if @root.nil?
+
+	    queue = [@root]
+	    while queue.any?
+	    	current_node = queue.shift
+	      	yield(current_node)
+	     	queue.concat(current_node.children)
+	    end
+	end
+
+	def dfs
+	    return if @root.nil?
+
+	    stack = [@root]
+	    while stack.any?
+	    	current_node = stack.pop
+	    	yield(current_node)
+	    	current_node.children.reverse.each { |child| stack.push(child) } 
 	    end
 	end
 end
