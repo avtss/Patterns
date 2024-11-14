b6bffeaac8c13e94daac10e77bc4b024a333fbec
avtss <avetiszadikyann@mail.ru>
2024-11-14 15:18:59 +0300

    Lab3.3. Added functions to work get info about tag

--- HTML Tree/HTMLTag.rb
+++ HTML Tree/HTMLTag.rb
@@ -12,4 +12,36 @@ class HtmlTag
 	def add_child(tag)
 		@children << tag if tag.is_a?(HtmlTag)
 	end
+
+  def count_children
+		children.size
+	end
+
+	def has_children?
+		!children.empty?
+	end
+
+	def to_s(indent = 0)
+		indentation = '  ' * indent
+		opening_tag = "<#{tag_name}#{attributes_string}>"
+		closing_tag = "</#{tag_name}>"
+
+		if content.nil? && children.empty?
+			"#{indentation}#{opening_tag}/>\n"
+		else
+			result = "#{indentation}#{opening_tag}\n"
+			result += "#{indentation}  #{content}\n" if content
+			children.each { |child| result += child.to_s(indent + 1) }
+			result += "#{indentation}#{closing_tag}\n"
+			result
+		end
+	end
+
+	def attributes_string
+		attributes.map { |key, value| " #{key}='#{value}'" }.join
+	end
+
+	def children_string
+		children.map(&:to_s).join
+	end
 end
