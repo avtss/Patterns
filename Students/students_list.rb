f6c8f140b6911f20577919f5be515dda2effe895
avtss <avetiszadikyann@mail.ru>
2024-11-30 22:54:21 +0300

    Lab4.2.4. Created superclass for students_list_json and students_list_yaml.

---
+++ Students/students_list.rb
@@ -0,0 +1,71 @@
+require_relative 'student'
+require_relative 'student_short'
+require_relative 'DataList/data_list_student_short'
+
+class Students_list
+    def initialize(file_path)
+        @file_path = file_path
+        @students = []
+        load_from_file
+    end
+
+    def load_from_file
+        raise NotImplementedError, "Метод не реализован в классе Students_list"
+    end
+
+    def save_to_file
+        raise NotImplementedError, "Метод не реализован в классе Students_list"
+    end
+
+    def find_student_by_id(id)
+        @students.find { |student| student.id == id }
+    end
+
+    def get_k_n_student_short_list(k, n, existing_data_list = nil)
+        start_index = (k - 1) * n
+        end_index = start_index + n - 1
+        short_students = @students[start_index..end_index] || []
+        return existing_data_list || Data_list_student_short.new([]) if short_students.empty?
+    
+        short_students = short_students.map { |student| Student_short.from_student(student) }
+        if existing_data_list
+          existing_data_list.data = short_students
+          short_students.each_with_index { |_, ind| existing_data_list.select(ind) }
+          return existing_data_list
+        end
+    
+        selected_list = Data_list_student_short.new(short_students)
+        short_students.each_with_index { |_, ind| selected_list.select(ind) }
+        selected_list
+      end
+
+      def sort_by_fullname
+        @students.sort_by!(&:fullname)
+      end
+
+      def add_student(student)
+        new_id = (@students.map(&:id).max || 0) + 1
+        student.id = new_id
+        @students << student
+        save_to_file
+      end
+    
+      def update_student_by_id(id, updated_student)
+        index = @students.find_index { |student| student.id == id }
+        return false unless index
+    
+        updated_student.id = id
+        @students[index] = updated_student
+        save_to_file
+        true
+      end
+
+      def delete_student_by_id(id)
+        @students.reject! { |student| student.id == id }
+        save_to_file
+    end
+
+    def count
+        @students.size
+      end
+end
--- Students/students_list_json.rb
+++ Students/students_list_json.rb
@@ -1,15 +1,7 @@
 require 'json'
-require_relative 'student'
-require_relative 'student_short'
-require_relative 'DataList/data_list_student_short'
+require_relative 'students_list.rb'
 
-class Students_list_JSON
-    def initialize(file_path)
-        @file_path = file_path
-        @students = []
-        load_from_file
-    end
-
+class Students_list_JSON < Students_list
     def load_from_file
         if File.exist?(@file_path)
           file_content = File.read(@file_path)
@@ -34,60 +26,6 @@ class Students_list_JSON
             birth_date: student.birth_date
           }
         end
-        File.open(@file_path, 'w') do |file|
-            file.write(JSON.pretty_generate(serialized_data))
-        end
+        File.open(@file_path, 'w') { |file| file.write(JSON.pretty_generate(serialized_data)) }
       end
-
-      def find_student_by_id(id)
-        @students.find { |student| student.id == id }
-      end
-
-      def get_k_n_student_short_list(k, n, existing_data_list = nil)
-        start_index = (k - 1) * n
-        end_index = start_index + n - 1
-        short_students = @students[start_index..end_index] || []
-        return existing_data_list || Data_list_student_short.new([]) if short_students.empty?
-        short_students = short_students.map { |student| Student_short.from_student(student) }
-      
-        if existing_data_list
-          existing_data_list.data = short_students
-          short_students.each_with_index { |_, ind| existing_data_list.select(ind) }
-          return existing_data_list
-        end
-        selected_list = Data_list_student_short.new(short_students)
-        short_students.each_with_index { |_, ind| selected_list.select(ind) }
-        selected_list
-      end
-      
-
-      def sort_by_fullname
-        @students.sort_by!(&:fullname)
-      end
-
-      def add_student(student)
-        new_id = (@students.map(&:id).max || 0) + 1
-        student.id = new_id
-        @students << student
-        save_to_file
-      end
-
-      def update_student_by_id(id, updated_student)
-        index = @students.find_index { |student| student.id == id }
-        return false unless index
-    
-        updated_student.id = id
-        @students[index] = updated_student
-        save_to_file
-        true
-    end
-
-    def delete_student_by_id(id)
-        @students.reject! { |student| student.id == id }
-        save_to_file
-    end
-
-    def count
-        @students.size
-    end
 end
