e7c080b45fd9735a19d349b1084a3ad0e4651585
avtss <avetiszadikyann@mail.ru>
2024-12-01 22:07:54 +0300

    Lab4.3.2. Modified for pgsql

--- Students/Database/structure/db.sql
+++ Students/Database/structure/db.sql
@@ -1,18 +1,13 @@
 CREATE DATABASE student;
-USE student;
 
 CREATE TABLE student (
-	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
     last_name TEXT NOT NULL,
     first_name TEXT NOT NULL,
     surname TEXT NOT NULL,
-    phone TEXT,
-    email TEXT,
-    telegram TEXT,
-    github TEXT,
+    phone TEXT UNIQUE,
+    email TEXT UNIQUE,
+    telegram TEXT UNIQUE,
+    github TEXT UNIQUE,
     birth_date DATE NOT NULL
 );
-
-ALTER TABLE student AUTO_INCREMENT = 1;
-
-ALTER TABLE student ADD UNIQUE (telegram(255)), ADD UNIQUE (email(255)), ADD UNIQUE (github(255)), ADD UNIQUE (phone(255));
