aea394fa86a6ba3ee65571fb7b66f914b7fb20b6
avtss <avetiszadikyann@mail.ru>
2024-12-13 13:36:18 +0300

    Lab4. Added tests for all new classes

--- Students/main.rb
+++ Students/main.rb
@@ -16,6 +16,51 @@ require_relative './DataStructures/ListAdapter/list_adapter'
 
 
 begin
+
+  sttree=Student_tree.new()
+  student1 = Student.new(
+  id: 1,
+  lastname: "Иванов",
+  firstname: "Иван",
+  surname: "Иванович",
+  phone: "+79991234567",
+  telegram: "@ivanov_ivan",
+  email: "ivanov.ivan@example.com",
+  github: "https://github.com/ivanov-ivan",
+  birth_date: "2000-01-15"
+)
+
+student2 = Student.new(
+  id: 2,
+  lastname: "Петров",
+  firstname: "Петр",
+  surname: "Петрович",
+  phone: "+79997654321",
+  telegram: "@petrov_petr",
+  email: "petrov.petr@example.com",
+  github: "https://github.com/petrov-petr",
+  birth_date: "1999-05-10"
+)
+student3 = Student.from_string("3; Сидоров; Сергей; Сергеевич; +79991112233; @sidorov_sergey; sidorov.sergey@example.com; https://github.com/sidorov-sergey; 1998-11-20")
+
+student4 = Student.from_hash(
+  id: 4,
+  lastname: "Кузнецов",
+  firstname: "Николай",
+  surname: "Николаевич",
+  phone: "+79994445566",
+  telegram: "@kuznetsov_nikolay",
+  email: "kuznetsov.nikolay@example.com",
+  github: "https://github.com/kuznetsov-nikolay",
+  birth_date: "2001-03-25"
+)
+  sttree.insert(student1)
+  sttree.insert(student2)
+  sttree.insert(student3)
+  sttree.insert(student4)
+  puts sttree.select{|s| s.lastname=="Кузнецов"}
+
+
   
 list_adapter1 = List_adapter.new(Students_list_adapter.new('students.yaml', YAML_Strategy.new))
 
@@ -33,6 +78,9 @@ list_adapter2 = List_adapter.new(Students_list_db_adapter.new(con))
 puts "Всего студентов в БД: #{list_adapter2.get_student_short_count}"
 student = list_adapter2.find_student_by_id(1)
 puts "Найден студент: #{student}"
+
+short_list=list_adapter2.get_k_n_student_short_list(2, 10)
+puts short_list.get_data
 rescue ArgumentError => e
   puts e.message
 end
