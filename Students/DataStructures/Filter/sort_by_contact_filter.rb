7ea5be79f7b20fea95f7b22461c308ac847052d2
avtss <avetiszadikyann@mail.ru>
2024-12-18 21:29:51 +0300

    Lab4.5. Removed unnecessarry filters

--- Students/DataStructures/Filter/sort_by_contact_filter.rb
+++
@@ -1,7 +0,0 @@
-require_relative './filter_decorator'
-
-class SortByContactFilter < FilterDecorator
-  def apply_filter(data)
-    super(data).sort_by { |student| student.contact || '' }
-  end
-end
